c6aee718984202c9ce32bdcf488b8d8a94a56631
whippet <whippet@plydot.com>
2020-03-16 14:30:41 +0300

    created the IUser model

--- app/src/main/java/com/chat/bposeats/data/data/entity/User.kt
+++ app/src/main/java/com/chat/bposeats/data/data/entity/User.kt
@@ -3,12 +3,26 @@ package com.chat.bposeats.data.data.entity
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.stfalcon.chatkit.commons.models.IUser
 
 @Entity(tableName = "Users", indices = [Index(value = ["phone"], unique = true)])
 data class User(
-    @PrimaryKey var id: String,
+    @PrimaryKey var _id: String,
     var username: String,
     var current: Boolean,
     var phone: String,
-    var name: String
-)
+    var _name: String,
+    var _avatar: String
+) : IUser {
+    override fun getAvatar(): String {
+        return _avatar
+    }
+
+    override fun getName(): String {
+        return _name
+    }
+
+    override fun getId(): String {
+        return _id
+    }
+}
